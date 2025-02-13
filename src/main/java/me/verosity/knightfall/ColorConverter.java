365c9e0f0b705f6299340f42cd194adb193fc227
Verosity <165721188+Verosity@users.noreply.github.com>
2025-02-12 16:53:35 -0800

    goonmax

---
+++ src/main/java/me/verosity/knightfall/ColorConverter.java
@@ -0,0 +1,15 @@
+package me.verosity.knightfall;
+
+import org.bukkit.Material;
+
+public class ColorConverter {
+
+    public static String stringToColorCode(String stringColor){
+
+        return "";
+    }
+
+    public static Material stringToBannerColor(){
+
+    }
+}
